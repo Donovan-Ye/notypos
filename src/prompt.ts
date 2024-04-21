5f457de99c0bd66ee19e9d782606d1bd45f56800
ZhengjieYe <xxiaozhiyzj@gmail.com>
2024-04-22 00:03:34 +0800

    feat: update prompt

--- src/prompt.ts
+++ src/prompt.ts
@@ -1,19 +1,15 @@
 const fixTyposPrompt = `
-Please help to identify any typos in the given content as much as possible.
+Please identify any typos and grammatical errors in the given content as much as possible, don't worry about making mistakes.
 Reply with the context, incorrect content, and correct content in the format of "{"context": "context", "err": "incorrect content", "correct": "correct content"}".
-Please keep the context including the whole line of the incorrect content.
+Please keep the context including orignal line.
 Add [END] after each error and continue.
 
 <Example>
 Assume the content is as follows:
-晚风吹过只剩星
 Nice to met you
-<!-- Badgs -->
 
 Reply:
-{"context": "晚风吹过只剩星", "err": "剩", "correct": "生"} [END]\n
 {"context": "Nice to met you", "err": "met", "correct": "meet"} [END]\n
-{"context": "<!-- Badgs -->", "err": "Badgs", "correct": "Badges"} [END]\n
 </Example>
 
 The user's content is as follows:
